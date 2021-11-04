7342654f245751bd0783962fe312e11d95f10bf9
Aibek <80278848+Aibek2002@users.noreply.github.com>
2021-11-04 11:51:52 +0500

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,21 @@
+try:
+   fhand = open('mailbox.txt')
+except:
+   print('File cannot be opened')
+   exit()
+
+lines=fhand.readlines()
+new_list=[]
+bibon=open('output.txt', 'w')
+for line in lines:
+   ind=line.find('[')
+   en_ind=line.find(']')
+   if line.startswith('Received: from') and 'unix' not in line[ind:en_ind]:
+      word=line[ind+1:en_ind]
+      if word not in new_list:
+         new_list.append(word)
+         new_list.sort()
+for item in new_list:
+   print(item)
+fhand.close()
+               
